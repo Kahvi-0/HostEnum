2fa13b260ded3a844ca7ee57edbbd3ace3a54e46
Kahvi-0xFF <46513413+Kahvi-0@users.noreply.github.com>
2023-04-03 12:54:02 -0400

    Update Search.ps1

--- Search.ps1
+++ Search.ps1
@@ -1,13 +1,4 @@
 # File names
-Get-ChildItem -Path C:\ -Recurse -exclude *.dll -erroraction 'silentlycontinue' -Filter "*pwds*" -File | % {Write-Host $_.FullName} >> WinPwds.txt
-Get-ChildItem -Path C:\ -Recurse -exclude *.dll -erroraction 'silentlycontinue' -Filter "*pass*" -File | % {Write-Host $_.FullName} >> WinPwds.txt
-Get-ChildItem -Path C:\ -Recurse -exclude *.dll -erroraction 'silentlycontinue' -Filter "*login*" -File | % {Write-Host $_.FullName} >> WinPwds.txt
-Get-ChildItem -Path C:\ -Recurse -exclude *.dll -erroraction 'silentlycontinue' -Filter "*mdp*" -File | % {Write-Host $_.FullName} >> WinPwds.txt
-
+Get-ChildItem -Path C:\ -Recurse -exclude *.dll -erroraction 'silentlycontinue' -Include "*pwds*","*pass*","*login*","*mdp*" -File | Foreach {Write-Host $_.FullName} >> WinPwds.txt
 # File contents
-Get-ChildItem -Path C:\ -Recurse -exclude *.dll, *.pdb,  *.cache, *.exe, *.gif, *.png, *.jpg, *.jpeg -erroraction 'silentlycontinue'  | select-string -Pattern "pwd.{1,12}"  -AllMatches -outvariable test  -erroraction 'silentlycontinue'  |  % { $_.Matches } |  % { $_.Value } >> WinPwds.txt
-Get-ChildItem -Path C:\ -Recurse -exclude *.dll, *.pdb,  *.cache, *.exe, *.gif, *.png, *.jpg, *.jpeg -erroraction 'silentlycontinue'  | select-string -Pattern "pass.{1,12}"  -AllMatches -outvariable test  -erroraction 'silentlycontinue'  |  % { $_.Matches } |  % { $_.Value } >> WinPwds.txt
-Get-ChildItem -Path C:\ -Recurse -exclude *.dll, *.pdb,  *.cache, *.exe, *.gif, *.png, *.jpg, *.jpeg -erroraction 'silentlycontinue'  | select-string -Pattern "-p .{1,12}"  -AllMatches -outvariable test  -erroraction 'silentlycontinue'  |  % { $_.Matches } |  % { $_.Value } >> WinPwds.txt
-Get-ChildItem -Path C:\ -Recurse -exclude *.dll, *.pdb,  *.cache, *.exe, *.gif, *.png, *.jpg, *.jpeg -erroraction 'silentlycontinue'  | select-string -Pattern "mot de passe.{1,12}"  -AllMatches -outvariable test  -erroraction 'silentlycontinue'  |  % { $_.Matches } |  % { $_.Value } >> WinPwds.txt
-Get-ChildItem -Path C:\ -Recurse -exclude *.dll, *.pdb,  *.cache, *.exe, *.gif, *.png, *.jpg, *.jpeg -erroraction 'silentlycontinue'  | select-string -Pattern "SAPWD.{1,12}"  -AllMatches -outvariable test  -erroraction 'silentlycontinue'  |  % { $_.Matches } |  % { $_.Value } >> WinPwds.txt
-Get-ChildItem -Path C:\ -Recurse -exclude *.dll, *.pdb,  *.cache, *.exe, *.gif, *.png, *.jpg, *.jpeg -erroraction 'silentlycontinue'  | select-string -Pattern "mdp.{1,12}"  -AllMatches -outvariable test  -erroraction 'silentlycontinue'  |  % { $_.Matches } |  % { $_.Value } >> WinPwds.txt
+Get-ChildItem -Path C:\ -Recurse -exclude *.docx, *.xslx, *.dll, *.pdb,  *.cache, *.exe, *.gif, *.png, *.jpg, *.jpeg -erroraction 'silentlycontinue'  | select-string -Pattern "pwd.{1,12}|pass.{1,12}|-p .{1,12}|SAPWD.{1,12}|mdp.{1,12}"  -AllMatches -outvariable test  -erroraction 'silentlycontinue' |  Foreach { $_.Matches }  |  % { $_.Value } >> WinPwds.txt
