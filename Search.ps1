764eda90b9c05ee94ba084700f3a6c0bd7a1a3d2
Kahvi-0xFF <46513413+Kahvi-0@users.noreply.github.com>
2023-04-03 11:58:28 -0400

    Update Search.ps1

--- Search.ps1
+++ Search.ps1
@@ -1,8 +1,8 @@
 # File names
-Get-ChildItem -Path . -Recurse -exclude *.dll -erroraction 'silentlycontinue' -Filter "*pwds*" -File | % {Write-Host $_.FullName} >> WinPwds.txt
-Get-ChildItem -Path . -Recurse -exclude *.dll -erroraction 'silentlycontinue' -Filter "*pass*" -File | % {Write-Host $_.FullName} >> WinPwds.txt
-Get-ChildItem -Path . -Recurse -exclude *.dll -erroraction 'silentlycontinue' -Filter "*login*" -File | % {Write-Host $_.FullName} >> WinPwds.txt
-Get-ChildItem -Path . -Recurse -exclude *.dll -erroraction 'silentlycontinue' -Filter "*mdp*" -File | % {Write-Host $_.FullName} >> WinPwds.txt
+Get-ChildItem -Path C:\ -Recurse -exclude *.dll -erroraction 'silentlycontinue' -Filter "*pwds*" -File | % {Write-Host $_.FullName} >> WinPwds.txt
+Get-ChildItem -Path C:\ -Recurse -exclude *.dll -erroraction 'silentlycontinue' -Filter "*pass*" -File | % {Write-Host $_.FullName} >> WinPwds.txt
+Get-ChildItem -Path C:\ -Recurse -exclude *.dll -erroraction 'silentlycontinue' -Filter "*login*" -File | % {Write-Host $_.FullName} >> WinPwds.txt
+Get-ChildItem -Path C:\ -Recurse -exclude *.dll -erroraction 'silentlycontinue' -Filter "*mdp*" -File | % {Write-Host $_.FullName} >> WinPwds.txt
 
 # File contents
 Get-ChildItem -Path C:\ -Recurse -exclude *.dll, *.pdb,  *.cache, *.exe, *.gif, *.png, *.jpg, *.jpeg -erroraction 'silentlycontinue'  | select-string -Pattern "pwd.{1,12}"  -AllMatches -outvariable test  -erroraction 'silentlycontinue'  |  % { $_.Matches } |  % { $_.Value } >> WinPwds.txt
